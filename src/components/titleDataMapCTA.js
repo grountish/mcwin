5ce01e62f84126760ec87e75c69da41626f667e8
Sofia Santa Maria <yolaresofia@gmail.com>
2024-03-11 15:40:59 +0100

    mobile polish for map component

--- src/components/titleDataMapCTA.js
+++ src/components/titleDataMapCTA.js
@@ -28,7 +28,7 @@ const TitleDataMapCTA = ({
         style={{ backgroundImage: `url(${urlFor(backgroundImage)})` }}
       >
         <div className='flex flex-col lg:flex-row pt-20 pb-32 justify-between'>
-          <div className='lg:w-1/2 w-full lg:pr-10'>
+          <div className='lg:w-1/2 w-full lg:pr-10 pb-10 lg:pb-0'>
             <div className='text-2xl'>
               <BlockContent blocks={topSecondaryTitle} />
             </div>
@@ -47,7 +47,12 @@ const TitleDataMapCTA = ({
                   alt='logo'
                   className='w-[20px]'
                 />
-                <BlockContent blocks={dataOne} className='text-xs lg:text-lg' />
+                <a href={dataOneURL}>
+                  <BlockContent
+                    blocks={dataOne}
+                    className='text-xs lg:text-lg'
+                  />
+                </a>
               </div>
               <div className='flex space-x-2 py-2'>
                 <img
@@ -78,7 +83,7 @@ const TitleDataMapCTA = ({
           </div>
           <div className='pt-7'>
             <iframe
-              className='rounded-2xl md:h-[450px] md:w-[600px] w-full h-[300px]'
+              className='rounded-2xl xl:h-[450px] xl:w-[600px] lg:h-[400px] lg:w-[450px] w-full h-[500px]'
               src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3035.965647058245!2d-3.7000110999999998!3d40.45389730000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd4229ec696e4141%3A0x16108830bed29888!2sGreen%20Patio!5e0!3m2!1ses-419!2ses!4v1710164830601!5m2!1ses-419!2ses'
               allowfullscreen=''
               loading='lazy'
