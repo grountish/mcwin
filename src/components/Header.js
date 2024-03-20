23fcb418da57819dd2fff4ea91514701627de70b
Sofia Santa Maria <yolaresofia@gmail.com>
2024-03-20 16:35:15 +0100

    remove animation from mainlogo in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -24,60 +24,60 @@ const Header = ({ layout }) => {
 
   const burgerIcon = (
     <svg
-      width="30"
-      height="25"
-      viewBox="0 0 30 25"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
+      width='30'
+      height='25'
+      viewBox='0 0 30 25'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
     >
-      <path d="M0 2H30" stroke="white" strokeWidth="2.5" />
-      <path d="M0 13H30" stroke="white" strokeWidth="2.5" />
-      <path d="M0 23H30" stroke="white" strokeWidth="2.5" />
+      <path d='M0 2H30' stroke='white' strokeWidth='2.5' />
+      <path d='M0 13H30' stroke='white' strokeWidth='2.5' />
+      <path d='M0 23H30' stroke='white' strokeWidth='2.5' />
     </svg>
   );
 
   const closeIcon = (
     <svg
-      width="55"
-      height="55"
-      viewBox="0 0 55 55"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
+      width='55'
+      height='55'
+      viewBox='0 0 55 55'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
     >
       <path
-        d="M14.6667 41.9559L13.0442 40.3334L25.8775 27.5L13.0442 14.6667L14.6667 13.0442L27.5 25.8775L40.3334 13.0442L41.9559 14.6667L29.1225 27.5L41.9559 40.3334L40.3334 41.9559L27.5 29.1225L14.6667 41.9559Z"
-        fill="white"
+        d='M14.6667 41.9559L13.0442 40.3334L25.8775 27.5L13.0442 14.6667L14.6667 13.0442L27.5 25.8775L40.3334 13.0442L41.9559 14.6667L29.1225 27.5L41.9559 40.3334L40.3334 41.9559L27.5 29.1225L14.6667 41.9559Z'
+        fill='white'
       />
     </svg>
   );
 
   return (
-    <div className="z-50 w-full bg-transparent absolute top-0 left-0 text-white">
-      <div className="flex items-center justify-between lg:px-20 px-6 lg:py-14 py-10">
-        <div className="flex lg:hidden items-center  lg:self-center justify-between h-16 lg:mr-auto cursor-pointer">
-          <img className="h-12" src={urlFor(mainLogo)} alt="primary logo" />
+    <div className='z-50 w-full bg-transparent absolute top-0 left-0 text-white'>
+      <div className='flex items-center justify-between lg:px-20 px-6 lg:py-14 py-10'>
+        <div className='flex lg:hidden items-center  lg:self-center justify-between h-16 lg:mr-auto cursor-pointer'>
+          <img className='h-12' src={urlFor(mainLogo)} alt='primary logo' />
         </div>
-        <div className="lg:hidden">
+        <div className='lg:hidden'>
           <button
             onClick={handleMenuOpen}
-            className="flex items-center p-3 navbar-burger"
+            className='flex items-center p-3 navbar-burger'
             data-menu
           >
             {burgerIcon}
           </button>
         </div>
-        <div className="hidden lg:flex items-center lg:self-center justify-between h-16 lg:mr-auto cursor-pointer">
-          <span onClick={() => router.push("/")} className="mr-auto">
+        <div className='hidden lg:flex items-center lg:self-center justify-between h-16 lg:mr-auto cursor-pointer'>
+          <span onClick={() => router.push("/")} className='mr-auto'>
             {mainLogo && (
               <img
-                className="h-16 md:h-20 fadeIn"
+                className='h-16 md:h-20'
                 src={urlFor(mainLogo)}
-                alt="primary logo"
+                alt='primary logo'
               />
             )}
           </span>
         </div>
-        <ul className="hidden lg:flex uppercase lg:items-center lg:w-auto lg:space-x-10 !list-none">
+        <ul className='hidden lg:flex uppercase lg:items-center lg:w-auto lg:space-x-10 !list-none'>
           {header.map(({ slug, title }, i) => (
             <Link
               key={i}
@@ -91,7 +91,7 @@ const Header = ({ layout }) => {
               {title}
             </Link>
           ))}
-          <span className="cursor-pointer underlined" onClick={register}>
+          <span className='cursor-pointer underlined' onClick={register}>
             REGISTER NOW
           </span>
         </ul>
@@ -103,19 +103,19 @@ const Header = ({ layout }) => {
         className={`fixed top-0 bottom-0 left-0 px-9 py-3 transition-all duration-[450ms] overflow-y-auto transform bg-black opacity-90 w-full z-100 block lg:hidden overflow-x-hidden ${
           !menuOpen ? "translate-x-[150vw]" : ""
         }`}
-        id="nav-wrapper"
+        id='nav-wrapper'
       >
-        <div className="flex items-center mb-8">
+        <div className='flex items-center mb-8'>
           <button
             onClick={closeMenu}
-            className="navbar-close z-10 pt-6"
+            className='navbar-close z-10 pt-6'
             data-menu
           >
             {closeIcon}
           </button>
         </div>
         <div>
-          <div className="text-right flex flex-col">
+          <div className='text-right flex flex-col'>
             {header.map(({ slug, title }, i) => (
               <Link
                 key={i}
@@ -128,7 +128,7 @@ const Header = ({ layout }) => {
               </Link>
             ))}
             <div
-              className="uppercase pt-4 font-regular text-lg"
+              className='uppercase pt-4 font-regular text-lg'
               onClick={register}
             >
               REGISTER NOW
