7a7561d05cdecccf2d7b991cc42ac3ce42108be9
Sofia Santa Maria <yolaresofia@gmail.com>
2024-03-22 15:16:13 +0100

    update logos ratio

--- src/components/ImageInfoCollection.js
+++ src/components/ImageInfoCollection.js
@@ -14,24 +14,24 @@ const ImageInfoCollection = ({
   moreSpeakersText,
 }) => {
   return (
-    <div className="font-regular">
+    <div className='font-regular'>
       <div
         className={`lg:pt-40 pt-12 bg-cover bg-no-repeat text-white relative`}
         style={{ backgroundImage: `url(${urlFor(backgroundImage)})` }}
       >
-        <div className="flex flex-col lg:flex-row pt-20 md:pb-32 lg:px-20 px-7 text-white pb-48">
-          <div className="lg:w-1/2 w-full lg:pr-10">
-            <div className="lg:text-6xl text-4xl font-poppinsExtraBold uppercase pb-6 anim">
+        <div className='flex flex-col lg:flex-row pt-20 md:pb-32 lg:px-20 px-7 text-white pb-48'>
+          <div className='lg:w-1/2 w-full lg:pr-10'>
+            <div className='lg:text-6xl text-4xl font-poppinsExtraBold uppercase pb-6 anim'>
               <BlockContent blocks={title} />
             </div>
-            <div className="anim">
-              <BlockContent blocks={subtitle} className="text-xs lg:text-lg" />
+            <div className='anim'>
+              <BlockContent blocks={subtitle} className='text-xs lg:text-lg' />
             </div>
             {CTATitle && (
-              <div className="pt-10" onClick={register}>
+              <div className='pt-10' onClick={register}>
                 <Button
                   title={CTATitle}
-                  classes="hover:bg-white hover:text-black fadeIn"
+                  classes='hover:bg-white hover:text-black fadeIn'
                 />
               </div>
             )}
@@ -39,42 +39,42 @@ const ImageInfoCollection = ({
         </div>
         <SeparatorRight />
       </div>
-      <div className="bg-matteWhite ">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 px-12 xl:px-72 lg:px-40 py-12">
+      <div className='bg-matteWhite '>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 px-12 xl:px-72 lg:px-40 py-12'>
           {collection.map(({ image, title, subtitle, logo }, index) => {
             return (
               <div
-                className="flex flex-col  items-center py-6 anim"
+                className='flex flex-col  items-center py-6 anim'
                 key={index}
               >
                 <div
-                  className="rounded-full h-40 w-40 bg-cover bg-center bg-no-repeat"
+                  className='rounded-full h-40 w-40 bg-cover bg-center bg-no-repeat'
                   style={{ backgroundImage: `url(${urlFor(image)})` }}
                 ></div>
-                <div className="text-mainBlue font-poppinsExtraBold text-lg pt-4">
+                <div className='text-mainBlue font-poppinsExtraBold text-lg pt-4'>
                   {title && <BlockContent blocks={title} />}
                 </div>
-                <div className="w-1/2 text-center text-xs pb-3">
+                <div className='w-1/2 text-center text-xs pb-3'>
                   {subtitle && <BlockContent blocks={subtitle} />}
                 </div>
                 <img
-                  className="max-h-28 max-w-[210px] min-w-[170px]"
+                  className='w-[40%] aspect-[3/2] object-contain'
                   src={urlFor(logo)}
-                  alt="company logo"
+                  alt='company logo'
                 />
               </div>
             );
           })}
         </div>
         {moreSpeakersText && (
-          <div className="w-full flex justify-center pb-10">
-            <div className="flex justify-center items-center flex-col">
+          <div className='w-full flex justify-center pb-10'>
+            <div className='flex justify-center items-center flex-col'>
               <img
-                src="/more-speakers.svg"
-                alt="more-speakers"
-                className="max-h-28 max-w-[210px] min-w-[170px]"
+                src='/more-speakers.svg'
+                alt='more-speakers'
+                className='max-h-28 max-w-[210px] min-w-[170px]'
               />
-              <h4 className="py-4 text-mainBlue text-xl">{moreSpeakersText}</h4>
+              <h4 className='py-4 text-mainBlue text-xl'>{moreSpeakersText}</h4>
             </div>
           </div>
         )}
