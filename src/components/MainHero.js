b1db58c37d7dd43300eaac9d2a87b891cd5ee8a7
yolaresofia <yolaresofia@gmail.com>
2025-03-19 17:44:39 +0100

    update button border width

--- src/components/MainHero.js
+++ src/components/MainHero.js
@@ -53,7 +53,7 @@ const MainHero = ({
             </div>
             <div className="pt-4 fadeIn">
               <div onClick={register}>
-                <button className="flex items-center lg:px-20 px-7 lg:py-5 py-3 w-max transition duration-300 rounded-full font-poppinsExtraBold lg:text-xl text-base cursor-pointer hover:drop-shadow-md z-50 text-darkBlue hover:text-deepBlue hover:border-deepBlue bg-transparent border-darkBlue border">
+                <button className="flex items-center lg:px-20 px-7 lg:py-5 py-3 w-max transition duration-300 rounded-full font-poppinsExtraBold lg:text-xl text-base cursor-pointer hover:drop-shadow-md z-50 text-darkBlue hover:text-deepBlue hover:border-deepBlue bg-transparent border-darkBlue border-2">
                   {buttonText}
                 </button>
               </div>
